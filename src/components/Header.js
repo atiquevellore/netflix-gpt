e5486140870c0cd8a1c1458cfef711d61bc9631a
atique <atiquevellore@gmail.com>
2024-05-11 19:33:54 +0530

    Header and login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Header = () => {
-	return <div></div>;
+	return (
+		<div className="absolute   z-10 w-48 px-8 py-2 bg-gradient-to-b  from-black">
+			<img
+				src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png"
+				alt="logo"
+			/>
+		</div>
+	);
 };
 
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Header";
 
 const Login = () => {
-	return <div>Login </div>;
+	const [isSign, SetIsSign] = useState(true);
+	function toggleSignInForm() {
+		SetIsSign(!isSign);
+	}
+	return (
+		<>
+			<Header />
+			<div className="absolute ">
+				<img
+					className=" bg-fixed"
+					src="https://assets.nflxext.com/ffe/siteui/vlv3/ff5587c5-1052-47cf-974b-a97e3b4f0656/065df910-dec3-46ae-afa8-7ad2b52dce40/IN-en-20240506-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+					alt="background image"
+				/>
+			</div>
+			<form className="  my-36 mx-auto right-0 left-0  w-3/12 absolute bg-black  text-white bg-opacity-80 rounded-lg">
+				<h1 className=" mx-10 py-3 my-2 text-3xl font-bold">
+					{isSign ? "Sign In" : "Sign Up"}
+				</h1>
+				{!isSign && (
+					<input
+						type="text"
+						placeholder="Full Name"
+						className="mx-10 p-3 my-3 w-4/5 bg-gray-600  bg-opacity-40 rounded-lg"
+					/>
+				)}
+				<input
+					type="email"
+					placeholder="Email"
+					className="mx-10 p-3 my-3 w-4/5 bg-gray-600  bg-opacity-40 rounded-lg"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					className="mx-10 p-3  my-3 w-4/5 bg-gray-600 bg-opacity-40 rounded-lg"
+				/>
+				<button className="mx-10 p-2 my-2 bg-red-600 w-4/5 rounded-lg text-lg ">
+					{isSign ? "Sign In" : "Sign Up"}
+				</button>
+				<p className="mx-10 p-4  cursor-pointer" onClick={toggleSignInForm}>
+					{isSign
+						? "New to Netflix? Sign Up Now"
+						: "Already Registered?, Sign In Now"}
+				</p>
+			</form>
+		</>
+	);
 };
 
 export default Login;
